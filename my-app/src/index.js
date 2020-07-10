c8fea2b3640b9d971b33b690e7b89b6d15b25d4a
Ubuntu <ubuntu@ip-172-31-47-118.ec2.internal>
2020-07-10 13:15:05 +0000

    Game board information configured.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -43,3 +43,25 @@ class Board extends React.Component {
     }
 }
 
+// Game board information
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    } 
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
