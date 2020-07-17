da72c38da427409eb17ef27a2ba4cde6e980d986
Ubuntu <ubuntu@ip-172-31-44-26.ec2.internal>
2020-07-17 15:38:13 +0000

    User comments updated to improve UXD.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,70 +2,72 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// x1 gaming square is configured to a button
+// Configure a square as a button function
 
 class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-            {/* TODO */}
-            </button>
-        );
-    }
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
 }
 
-// A 3x3 gaming board constructed fom index 0 to 8
+// Create gaming board. Squares based on index scoring from 0 to 8
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square />;
-    }
-    
-    render() {
-        const status = 'Next Player: X';
-        
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
 }
 
-// Game board information
+// Game status and user information
 
 class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    } 
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 
+// ========================================
+
 ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+  <Game />,
+  document.getElementById('root')
 );
