68e28f5cd15bcd63522b0a0366924f9e52dc07d0
Ubuntu <paul.b.friel@me.com>
2020-07-20 14:52:52 +0000

    Square render method updated to refer to a property value. Board render method updated to allocate a value to each square.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,7 +8,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -18,7 +18,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
