1fca2183ab2ad6972b69e8263b21e389f845d417
Ubuntu <paul.b.friel@me.com>
2020-07-29 22:45:31 +0000

    Board component render updated to add constant variables history and current. Game component updated to update game-board to ensure onClick is managed at the parent Game level.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -48,10 +48,12 @@ class Board extends React.Component {
 
   // Text produced to confirm the Crater-Cross champion
   render() {
-    const winner = calculateWinner(this.state.squares);
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
     let status;
     if (winner) {
-      status = 'Champion: 3 in a row!!';
+      status = 'Winner: ' + winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -96,7 +98,10 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
