14c5126f3142135f774208544a2b24098025b7a0
Ubuntu <paul.b.friel@me.com>
2020-08-03 13:59:35 +0000

    jumpTo method added to Game component. This method supports the user tracking back through game history.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -88,6 +88,13 @@ class Game extends React.Component {
     });
   }
   
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+  
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
