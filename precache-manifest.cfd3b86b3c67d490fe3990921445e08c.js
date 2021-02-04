b9388731a69d163dccf2ba25aa8e75687d9cdc46
Ubuntu <ubuntu@ip-172-31-34-237.eu-west-1.compute.internal>
2021-02-04 19:34:08 +0000

    Updates

---
+++ precache-manifest.cfd3b86b3c67d490fe3990921445e08c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ceb2d59fd3126f89841c9742f1e4eb",
+    "url": "/Crater-Cross/index.html"
+  },
+  {
+    "revision": "93a7b0c3f9039701838a",
+    "url": "/Crater-Cross/static/css/main.12e78e45.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/Crater-Cross/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Crater-Cross/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a7b0c3f9039701838a",
+    "url": "/Crater-Cross/static/js/main.cb699a6d.chunk.js"
+  },
+  {
+    "revision": "7539286ef31dbdc670db",
+    "url": "/Crater-Cross/static/js/runtime-main.40fc318b.js"
+  }
+]);
